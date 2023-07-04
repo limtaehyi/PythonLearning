9752b4cad6bcaebd3f1b606f530600a3b9d3588d
lim-taehe <lt75355@naver.com>
2023-07-04 16:58:48 +0900

    2023.07.04

--- test/dice.py
+++ test/dice.py
@@ -1,15 +1,35 @@
+import random
 #-*- encoding: utf-8 -*-
 
 def main():
+    number_list = [1,2,3,4,5,6]
+    
+    top = int(input(">"))
+    bottom = 7 - top
+
+    number_list.remove(top)
+    number_list.remove(bottom)
+
+    left = random.choice(number_list)
+    right = 7 - left
+
+    number_list.remove(left)
+    number_list.remove(right)
+
+    body = number_list[0]
+    foot = number_list[1]
+
+    
+    
     print(f"""
                     ---
-                   | {} |
+                   | {top} |
                 --- --- ---
-               | {} | {} | {} |
+               | {left} | {body} | {right} |
                 --- --- ---
-                   | {} |
+                   | {bottom} |
                     ---
-                   | {} |
+                   | {foot} |
                     ---
 """)
 
