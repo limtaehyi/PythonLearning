894d7eb527bbfec44cdc47364710d36d2b432d3b
lim-taehe <lt75355@naver.com>
2023-07-10 13:48:25 +0900

    2023.07.10

--- test/coins.py
+++ test/coins.py
@@ -2,8 +2,8 @@
 
 def main():
 
-    print(f"500원 : {}, 100원 : {}, 50원 : {}, 10원 : {}")
-    print(f"거스름돈 동전의 최소 개수는 {}개 입니다.")
+    print(f"500원 : {5baek}개, 100원 : {1baek}개, 50원 : {5ship}개, 10원 : {1ship}개")
+    print(f"거스름돈 동전의 최소 개수는 총 {result_coins}개 입니다.")
 
 if __name__ == "__main__":
     main()
--- test/decimal.py
+++ test/decimal.py
@@ -1,10 +1,10 @@
 #-*- encoding: utf-8 -*-
 
 def main():
-    print(f"{}은 소수입니다.")
-    print()
-    print(f"{}은 소수가 아닙니다.")
-    print()
+    print(f"{input_val}은 소수입니다.")
+    print(decimal_list)
+    print(f"{input_val}은 소수가 아닙니다.")
+    print(decimal_list)
 
 if __name__ == "__main__":
     main()
