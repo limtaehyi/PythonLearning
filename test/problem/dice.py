8eaa03cb3c9ab81e0b4383e3909c174ccf2dab7f
lim-taehe <lt75355@naver.com>
2023-07-10 15:19:39 +0900

    2023.07.10

---
+++ test/problem/dice.py
@@ -0,0 +1,18 @@
+#-*- encoding: utf-8 -*-
+
+
+def main():
+    print(f"""
+                    ---
+                   | {top} |
+                --- --- ---
+               | {left} | {body} | {right} |
+                --- --- ---
+                   | {bottom} |
+                    ---
+                   | {foot} |
+                    ---
+""")
+
+if __name__ == "__main__":
+    main()
