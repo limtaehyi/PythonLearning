974c11c2bc9516c43d4aa52593308ca2fdb12f12
lim-taehe <lt75355@naver.com>
2023-07-10 14:57:48 +0900

    2023.07.10

--- test/coins.py
+++ test/coins.py
@@ -2,7 +2,8 @@
 
 def main():
 
-    print(f"500원 : {5baek}개, 100원 : {1baek}개, 50원 : {5ship}개, 10원 : {1ship}개")
+    print(f"{result_money}의 거스름돈이 나왔습니다.")
+    print(f"500원 : {baek5}개, 100원 : {baek1}개, 50원 : {ship5}개, 10원 : {ship1}개")
     print(f"거스름돈 동전의 최소 개수는 총 {result_coins}개 입니다.")
 
 if __name__ == "__main__":
